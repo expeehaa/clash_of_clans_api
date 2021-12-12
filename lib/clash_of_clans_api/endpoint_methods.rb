e554590a0f284dd2b82f0e254be77ba589e177be
expeehaa <expeehaa@outlook.com>
2021-12-12 19:36:28 +0100

    Remove dedicated post and get methods.

--- lib/clash_of_clans_api/endpoint_methods.rb
+++ lib/clash_of_clans_api/endpoint_methods.rb
@@ -8,32 +8,29 @@ module ClashOfClansApi
 			raise NotImplementedError
 		end
 		
-		def perform_request(api_path, query: nil, &block)
+		def perform_request(method, api_path, query: nil, body: nil)
 			uri = self.class::BASE_URI+api_path
 			uri.query = URI.encode_www_form(query) if query
 			
 			Net::HTTP.start(uri.host, uri.port, use_ssl: uri.scheme=='https') do |http|
-				block.call(uri).then do |request|
+				case method
+				when :get
+					Net::HTTP::Get
+				when :post
+					Net::HTTP::Post
+				else
+					raise ArgumentError, "Invalid method #{method.inspect}."
+				end.new(uri).tap do |request|
 					endpoint_headers.each do |name, value|
 						request[name] = value
 					end
 					
+					if body
+						request['Content-Type'] = 'application/json'
+						request.body            = body
+					end
+				end.then do |request|
 					http.request(request)
-				end
-			end
-		end
-		
-		def perform_get(api_path, query: nil)
-			perform_request(api_path, query: query) do |request_uri|
-				Net::HTTP::Get.new(request_uri)
-			end
-		end
-		
-		def perform_post(api_path, body, query: nil)
-			perform_request(api_path, query: query) do |request_uri|
-				Net::HTTP::Post.new(request_uri).tap do |request|
-					request['Content-Type'] = 'application/json'
-					request.body            = body
 				end
 			end
 		end
@@ -58,14 +55,14 @@ module ClashOfClansApi
 					define_method(name) do |*args, **kwargs|
 						uri = endpoint.respond_to?(:call) ? endpoint.call(*args, **kwargs) : endpoint
 						
-						transform_response(perform_get(uri, query: kwargs.dig(:query)))
+						transform_response(perform_request(:get, uri, query: kwargs.dig(:query)))
 					end
 				when :post
 					define_method(name) do |*args, **kwargs|
 						uri          = endpoint.respond_to?(:call) ? endpoint.call(*args, **kwargs) : endpoint
 						request_body = body.call(*args, **kwargs)
 						
-						transform_response(perform_post(uri, request_body, query: kwargs.dig(:query)))
+						transform_response(perform_request(:post, uri, body: request_body, query: kwargs.dig(:query)))
 					end
 				else
 					raise "Unsupported argument 'method: #{method.inspect}'"
