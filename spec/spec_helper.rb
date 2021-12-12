c2e26e50c1fcaa55441fd6b2e0fff20b497083ee
expeehaa <expeehaa@outlook.com>
2021-12-12 23:03:45 +0100

    Set up VCR for use in tests.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,16 @@ require 'pry'
 require 'pry-byebug'
 require 'clash_of_clans_api'
 
+require 'vcr'
+
+VCR.configure do |config|
+	config.hook_into :webmock
+	config.cassette_library_dir     = File.expand_path('cassettes', __dir__)
+	config.default_cassette_options = {
+		record: ENV.fetch('VCR_RECORD', 'false') == 'true' ? :all : :none
+	}
+end
+
 RSpec.configure do |config|
 	# Enable flags like --only-failures and --next-failure
 	config.example_status_persistence_file_path = '.rspec_status'
@@ -13,5 +23,15 @@ RSpec.configure do |config|
 	
 	config.expect_with :rspec do |c|
 		c.syntax = :expect
+	end
+	
+	config.around do |example|
+		if example.metadata[:vcr_cassette]
+			VCR.use_cassette(example.metadata[:vcr_cassette]) do
+				example.run
+			end
+		else
+			example.run
+		end
 	end
 end
