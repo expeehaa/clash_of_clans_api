574785169d1f80fde040bfa6f3f7d0b6ea4f3fc4
expeehaa <expeehaa@outlook.com>
2021-12-23 18:03:29 +0100

    Add method ClashOfClansApi::Utils.call_proc_without_unknown_keywords.

--- lib/clash_of_clans_api/utils.rb
+++ lib/clash_of_clans_api/utils.rb
@@ -7,5 +7,22 @@ module ClashOfClansApi
 				raise TypeError, 'cannot escape nil'
 			end
 		end
+		
+		def self.call_proc_without_unknown_keywords(proc, *args, **kwargs, &block)
+			params = proc.parameters.group_by(&:first).transform_values! do |m|
+				m.map do |s|
+					s[1]
+				end
+			end
+			
+			proc_keys =
+				if params.key?(:keyrest)
+					kwargs
+				else
+					kwargs.slice(*(params.values_at(:key, :keyreq).compact.flatten))
+				end
+			
+			proc.call(*args, **proc_keys, &block)
+		end
 	end
 end
--- spec/clash_of_clans_api/utils_spec.rb
+++ spec/clash_of_clans_api/utils_spec.rb
@@ -13,4 +13,43 @@ RSpec.describe ClashOfClansApi::Utils do
 			expect(ClashOfClansApi::Utils.url_escape({a: 2, b: 5})).to eq '%7B%3Aa%3D%3E2%2C+%3Ab%3D%3E5%7D'
 		end
 	end
+	
+	describe '.call_proc_without_unknown_keywords' do
+		it 'has the same behaviour as Proc#call when no unknown keywords are defined' do
+			expect(                                                          proc{ |a                    | a               }.call               ).to eq nil
+			expect(ClashOfClansApi::Utils.call_proc_without_unknown_keywords(proc{ |a                    | a               }                   )).to eq nil
+			expect(                                                          proc{ |a                    | a               }.call(1, 2, 3      )).to eq 1
+			expect(ClashOfClansApi::Utils.call_proc_without_unknown_keywords(proc{ |a                    | a               },     1, 2, 3      )).to eq 1
+			expect(                                                          proc{ |a, *args             | [a, args]       }.call(1, 2, 3      )).to eq [1, [2, 3]]
+			expect(ClashOfClansApi::Utils.call_proc_without_unknown_keywords(proc{ |a, *args             | [a, args]       },     1, 2, 3      )).to eq [1, [2, 3]]
+			
+			expect{                                                          proc{ |                y:   |                 }.call               }.to raise_error ArgumentError, 'missing keyword: :y'
+			expect{ClashOfClansApi::Utils.call_proc_without_unknown_keywords(proc{ |                y:   |                 }                   )}.to raise_error ArgumentError, 'missing keyword: :y'
+			expect{                                                          proc{ |a, *args,       y:   |                 }.call               }.to raise_error ArgumentError, 'missing keyword: :y'
+			expect{ClashOfClansApi::Utils.call_proc_without_unknown_keywords(proc{ |a, *args,       y:   |                 }                   )}.to raise_error ArgumentError, 'missing keyword: :y'
+			expect{                                                          proc{ |a, *args,       y:   |                 }.call(         x: 4)}.to raise_error ArgumentError, 'missing keyword: :y'
+			expect{ClashOfClansApi::Utils.call_proc_without_unknown_keywords(proc{ |a, *args,       y:   |                 },              x: 4)}.to raise_error ArgumentError, 'missing keyword: :y'
+			expect(                                                          proc{ |a, *args, x:         | [a, args, x]    }.call(1, 2, 3, x: 4)).to eq [1, [2, 3], 4]
+			expect(ClashOfClansApi::Utils.call_proc_without_unknown_keywords(proc{ |a, *args, x:         | [a, args, x]    },     1, 2, 3, x: 4)).to eq [1, [2, 3], 4]
+			expect(                                                          proc{ |a, *args, x: 2       | [a, args, x]    }.call(1, 2, 3, x: 4)).to eq [1, [2, 3], 4]
+			expect(ClashOfClansApi::Utils.call_proc_without_unknown_keywords(proc{ |a, *args, x: 2       | [a, args, x]    },     1, 2, 3, x: 4)).to eq [1, [2, 3], 4]
+			expect{                                                          proc{ |a, *args, x: 2, y:   |                 }.call(         x: 4)}.to raise_error ArgumentError, 'missing keyword: :y'
+			expect{ClashOfClansApi::Utils.call_proc_without_unknown_keywords(proc{ |a, *args, x: 2, y:   |                 },              x: 4)}.to raise_error ArgumentError, 'missing keyword: :y'
+			expect(                                                          proc{ |a, *args, x: 2, y: 5 | [a, args, x, y] }.call(1, 2, 3, x: 4)).to eq [1, [2, 3], 4, 5]
+			expect(ClashOfClansApi::Utils.call_proc_without_unknown_keywords(proc{ |a, *args, x: 2, y: 5 | [a, args, x, y] },     1, 2, 3, x: 4)).to eq [1, [2, 3], 4, 5]
+		end
+		
+		it 'does not pass unknown keywords to Proc#call' do
+			expect(                                                          proc{ |                      |        }.call(x: 4      )).to eq nil
+			expect(ClashOfClansApi::Utils.call_proc_without_unknown_keywords(proc{ |                      |        },     x: 4      )).to eq nil
+			expect(                                                          proc{ |a, *args              |        }.call(x: 4      )).to eq nil
+			expect(ClashOfClansApi::Utils.call_proc_without_unknown_keywords(proc{ |a, *args              |        },     x: 4      )).to eq nil
+			expect(                                                          proc{ |a, *args, x:          | x      }.call(x: 4      )).to eq 4
+			expect(ClashOfClansApi::Utils.call_proc_without_unknown_keywords(proc{ |a, *args, x:          | x      },     x: 4      )).to eq 4
+			expect{                                                          proc{ |a, *args, x:          | x      }.call(x: 4, y: 2)}.to raise_error ArgumentError, 'unknown keyword: :y'
+			expect(ClashOfClansApi::Utils.call_proc_without_unknown_keywords(proc{ |a, *args, x:          | x      },     x: 4, y: 2)).to eq 4
+			expect(                                                          proc{ |a, *args, x:, **kwargs| kwargs }.call(x: 4, y: 2)).to eq({y: 2})
+			expect(ClashOfClansApi::Utils.call_proc_without_unknown_keywords(proc{ |a, *args, x:, **kwargs| kwargs },     x: 4, y: 2)).to eq({y: 2})
+		end
+	end
 end
