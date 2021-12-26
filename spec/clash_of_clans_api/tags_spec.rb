db088078709660393bb6632bd0e444fd89e50754
expeehaa <expeehaa@outlook.com>
2021-12-26 22:32:34 +0100

    Change leaking constants to let definitions.

--- spec/clash_of_clans_api/tags_spec.rb
+++ spec/clash_of_clans_api/tags_spec.rb
@@ -1,145 +1,151 @@
 require 'spec_helper'
 
 RSpec.describe ClashOfClansApi::Tags do
-	PROPER_TAGS = %w[
-		#29YLJCUL0
-		#8GG8VGQGR
-		#898YCPRQ2
-		#Q29GYVCL
-		#Q28PVLG8
-		#9GYP292CY
-		#89C9UU9C8
-		#8P8P9RRRU
-		#URLQVUVQ
-		#8PYL89Q0L
-		#8L89PJPP
-		#2PPULRUU9
-		#290LR8RLG
-		#8QJY9V8P
-		#2Y8YVGP8V
-		#9GP02C22
-		#PV8QPQLG
-		#28C8V8GP2
-		#8QUY8RPR
-		#2YYLC0VRP
-		#P
-		#Y
-		#L
-		#Q
-		#G
-		#R
-		#J
-		#C
-		#U
-		#V
-		#0
-		#2
-		#8
-		#9
-	]
+	let(:proper_tags) do
+		%w[
+			#29YLJCUL0
+			#8GG8VGQGR
+			#898YCPRQ2
+			#Q29GYVCL
+			#Q28PVLG8
+			#9GYP292CY
+			#89C9UU9C8
+			#8P8P9RRRU
+			#URLQVUVQ
+			#8PYL89Q0L
+			#8L89PJPP
+			#2PPULRUU9
+			#290LR8RLG
+			#8QJY9V8P
+			#2Y8YVGP8V
+			#9GP02C22
+			#PV8QPQLG
+			#28C8V8GP2
+			#8QUY8RPR
+			#2YYLC0VRP
+			#P
+			#Y
+			#L
+			#Q
+			#G
+			#R
+			#J
+			#C
+			#U
+			#V
+			#0
+			#2
+			#8
+			#9
+		]
+	end
 	
-	SANITIZABLE_TAGS = [
-		'  #29YLJCUL0  ',
-		'29YLJCUL0',
-		'#29YLJCULO',
-		'#29yljcul0',
-		'  29yljculo    ',
-	]
+	let(:sanitizable_tags) do
+		[
+			'  #29YLJCUL0  ',
+			'29YLJCUL0',
+			'#29YLJCULO',
+			'#29yljcul0',
+			'  29yljculo    ',
+		]
+	end
 	
-	WRONG_TAGS = %w[
-		*29YLJCUL0
-		#A
-		#B
-		#D
-		#E
-		#F
-		#H
-		#I
-		#K
-		#M
-		#N
-		#S
-		#T
-		#W
-		#X
-		#Z
-		#1
-		#3
-		#4
-		#5
-		#6
-		#7
-	]
+	let(:wrong_tags) do
+		%w[
+			*29YLJCUL0
+			#A
+			#B
+			#D
+			#E
+			#F
+			#H
+			#I
+			#K
+			#M
+			#N
+			#S
+			#T
+			#W
+			#X
+			#Z
+			#1
+			#3
+			#4
+			#5
+			#6
+			#7
+		]
+	end
 	
 	describe '.tag_regex' do
-		it 'matches all PROPER_TAGS' do
-			PROPER_TAGS.each do |tag|
+		it 'matches all proper_tags' do
+			proper_tags.each do |tag|
 				expect(tag).to match ClashOfClansApi::Tags.tag_regex
 			end
 		end
 		
-		it 'does not match any SANITIZABLE_TAGS' do
-			SANITIZABLE_TAGS.each do |tag|
+		it 'does not match any sanitizable_tags' do
+			sanitizable_tags.each do |tag|
 				expect(tag).not_to match ClashOfClansApi::Tags.tag_regex
 			end
 		end
 		
-		it 'does not match any WRONG_TAGS' do
-			WRONG_TAGS.each do |tag|
+		it 'does not match any wrong_tags' do
+			wrong_tags.each do |tag|
 				expect(tag).not_to match ClashOfClansApi::Tags.tag_regex
 			end
 		end
 	end
 	
 	describe '.sanitizable_tag_regex' do
-		it 'matches all PROPER_TAGS' do
-			PROPER_TAGS.each do |tag|
+		it 'matches all proper_tags' do
+			proper_tags.each do |tag|
 				expect(tag).to match ClashOfClansApi::Tags.sanitizable_tag_regex
 			end
 		end
 		
-		it 'matches all SANITIZABLE_TAGS' do
-			SANITIZABLE_TAGS.each do |tag|
+		it 'matches all sanitizable_tags' do
+			sanitizable_tags.each do |tag|
 				expect(tag).to match ClashOfClansApi::Tags.sanitizable_tag_regex
 			end
 		end
 		
-		it 'does not match any WRONG_TAGS' do
-			WRONG_TAGS.each do |tag|
+		it 'does not match any wrong_tags' do
+			wrong_tags.each do |tag|
 				expect(tag).not_to match ClashOfClansApi::Tags.sanitizable_tag_regex
 			end
 		end
 	end
 	
 	describe '.sanitizable?' do
-		it 'returns true for all PROPER_TAGS' do
-			PROPER_TAGS.each do |tag|
+		it 'returns true for all proper_tags' do
+			proper_tags.each do |tag|
 				expect(ClashOfClansApi::Tags.sanitizable?(tag)).to eq true
 			end
 		end
 		
-		it 'returns true for all SANITIZABLE_TAGS' do
-			SANITIZABLE_TAGS.each do |tag|
+		it 'returns true for all sanitizable_tags' do
+			sanitizable_tags.each do |tag|
 				expect(ClashOfClansApi::Tags.sanitizable?(tag)).to eq true
 			end
 		end
 		
-		it 'returns false for all WRONG_TAGS' do
-			WRONG_TAGS.each do |tag|
+		it 'returns false for all wrong_tags' do
+			wrong_tags.each do |tag|
 				expect(ClashOfClansApi::Tags.sanitizable?(tag)).to eq false
 			end
 		end
 	end
 	
 	describe '.sanitize' do
-		it 'returns all PROPER_TAGS unchanged' do
-			PROPER_TAGS.each do |tag|
+		it 'returns all proper_tags unchanged' do
+			proper_tags.each do |tag|
 				expect(ClashOfClansApi::Tags.sanitize(tag)).to eq tag
 			end
 		end
 		
-		it 'sanitizes all SANITIZABLE_TAGS' do
-			SANITIZABLE_TAGS.each do |tag|
+		it 'sanitizes all sanitizable_tags' do
+			sanitizable_tags.each do |tag|
 				expect(ClashOfClansApi::Tags.sanitize(tag)).to eq '#29YLJCUL0'
 			end
 		end
