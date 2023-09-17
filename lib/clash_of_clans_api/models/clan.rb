3c3b585eccb37d901a65dfbafa240bc632c35af7
expeehaa <expeehaa@outlook.com>
2023-09-17 23:55:05 +0200

    Fix alignment.

--- lib/clash_of_clans_api/models/clan.rb
+++ lib/clash_of_clans_api/models/clan.rb
@@ -10,35 +10,35 @@ require_relative 'war_league'
 module ClashOfClansApi
 	module Models
 		class Clan < Base
-			property :tag,                      'tag',                    required: true
-			property :name,                     'name',                   required: true
-			property :type,                     'type'
-			property :description,              'description'
-			property :location,                 'location',               type: ClanLocation
-			property :family_friendly?,         'isFamilyFriendly'
-			property :badge_urls,               'badgeUrls',              type: IconSet
-			property :clan_level,               'clanLevel'
-			property :clan_points,              'clanPoints'
-			property :clan_builder_base_points, 'clanBuilderBasePoints'
-			property :clan_versus_points,       'clanVersusPoints'
-			property :clan_capital_points,      'clanCapitalPoints'
-			property :capital_league,           'capitalLeague',          type: CapitalLeague
-			property :required_trophies,        'requiredTrophies'
-			property :war_frequency,            'warFrequency'
-			property :war_win_streak,           'warWinStreak'
-			property :war_wins,                 'warWins'
-			property :war_ties,                 'warTies'
-			property :war_losses,               'warLosses'
-			property :war_log_public?,          'isWarLogPublic'
-			property :war_league,               'warLeague',              type: WarLeague
-			property :members,                  'members'
-			property :member_list,              'memberList',             type: 'ClashOfClansApi::Models::Player'
-			property :labels,                   'labels',                 type: Label
+			property :tag,                            'tag',  required: true
+			property :name,                           'name', required: true
+			property :type,                           'type'
+			property :description,                    'description'
+			property :location,                       'location',                    type: ClanLocation
+			property :family_friendly?,               'isFamilyFriendly'
+			property :badge_urls,                     'badgeUrls',                   type: IconSet
+			property :clan_level,                     'clanLevel'
+			property :clan_points,                    'clanPoints'
+			property :clan_builder_base_points,       'clanBuilderBasePoints'
+			property :clan_versus_points,             'clanVersusPoints'
+			property :clan_capital_points,            'clanCapitalPoints'
+			property :capital_league,                 'capitalLeague',               type: CapitalLeague
+			property :required_trophies,              'requiredTrophies'
+			property :war_frequency,                  'warFrequency'
+			property :war_win_streak,                 'warWinStreak'
+			property :war_wins,                       'warWins'
+			property :war_ties,                       'warTies'
+			property :war_losses,                     'warLosses'
+			property :war_log_public?,                'isWarLogPublic'
+			property :war_league,                     'warLeague',                   type: WarLeague
+			property :members,                        'members'
+			property :member_list,                    'memberList',                  type: 'ClashOfClansApi::Models::Player'
+			property :labels,                         'labels',                      type: Label
 			property :required_builder_base_trophies, 'requiredBuilderBaseTrophies'
-			property :required_versus_trophies, 'requiredVersusTrophies'
-			property :required_townhall_level,  'requiredTownhallLevel'
-			property :clan_capital,             'clanCapital',            type: ClanCapital
-			property :chat_language,            'chatLanguage',           type: ChatLanguage
+			property :required_versus_trophies,       'requiredVersusTrophies'
+			property :required_townhall_level,        'requiredTownhallLevel'
+			property :clan_capital,                   'clanCapital',                 type: ClanCapital
+			property :chat_language,                  'chatLanguage',                type: ChatLanguage
 		end
 	end
 end
