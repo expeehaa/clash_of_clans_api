00024c169310737e66d966617fa11fb985472cbe
expeehaa <expeehaa@outlook.com>
2023-09-17 17:00:54 +0200

    Combine endpoint method tests with and without arguments.

--- spec/clash_of_clans_api/api_spec.rb
+++ spec/clash_of_clans_api/api_spec.rb
@@ -34,19 +34,27 @@ RSpec.describe ClashOfClansApi::Api do
 		[:clan_members,                         ['#2YYQPVGQQ'], ['aninvalidvalue']],
 		[:clan_capitalraidseasons,              ['#2YYQPVGQQ'], ['aninvalidvalue']],
 		[:player,                               ['#QG8VUCRUQ'], ['aninvalidvalue']],
+		[:capitalleagues,                       [            ], [                ]],
+		[:leagues,                              [            ], [                ]],
 		[:capitalleague,                        [85000022    ], ['aninvalidvalue']],
+		[:builderbaseleague,                    [44000041    ], ['aninvalidvalue']],
+		[:builderbaseleagues,                   [            ], [                ]],
 		[:league,                               [29000000    ], ['aninvalidvalue']],
 		[:league_seasons,                       [29000022    ], ['aninvalidvalue']], # League seasons are only available in the legendary league.
-		[:builderbaseleague,                    [44000041    ], ['aninvalidvalue']],
 		[:warleague,                            [48000000    ], ['aninvalidvalue']],
+		[:warleagues,                           [            ], [                ]],
 		[:location_rankings_clans,              [32000094    ], ['aninvalidvalue']],
 		[:location_rankings_players,            [32000094    ], ['aninvalidvalue']],
 		[:location_rankings_clansversus,        [32000094    ], ['aninvalidvalue']],
 		[:location_rankings_playersbuilderbase, [32000094    ], ['aninvalidvalue']],
 		[:location_rankings_clansbuilderbase,   [32000094    ], ['aninvalidvalue']],
 		[:location_rankings_playersversus,      [32000094    ], ['aninvalidvalue']],
+		[:locations,                            [            ], [                ]],
 		[:location_rankings_capitals,           [32000094    ], ['aninvalidvalue']],
 		[:location,                             [32000000    ], ['aninvalidvalue']],
+		[:goldpass_seasons_current,             [            ], [                ]],
+		[:labels_players,                       [            ], [                ]],
+		[:labels_clans,                         [            ], [                ]],
 	].each do |method_name, valid_arguments, invalid_arguments|
 		describe "##{method_name}", vcr_cassette: method_name do
 			it 'accepts query parameters' do
@@ -56,10 +64,12 @@ RSpec.describe ClashOfClansApi::Api do
 				expect(api.send(method_name, ['some_test_string']*invalid_arguments.size, query: {test: 5, asdf: 'i like trains'})).to eq 'flying cows'
 			end
 			
-			it 'raises an error with an invalid argument' do
-				expect{api.send(method_name, *invalid_arguments)}.to raise_error do |error|
-					expect(error         ).to be_a ApiFrame::NoSuccessError
-					expect(error.response).to be_a(Net::HTTPNotFound).or be_a(Net::HTTPBadRequest)
+			if invalid_arguments.any?
+				it 'raises an error with an invalid argument' do
+					expect{api.send(method_name, *invalid_arguments)}.to raise_error do |error|
+						expect(error         ).to be_a ApiFrame::NoSuccessError
+						expect(error.response).to be_a(Net::HTTPNotFound).or be_a(Net::HTTPBadRequest)
+					end
 				end
 			end
 			
@@ -70,32 +80,6 @@ RSpec.describe ClashOfClansApi::Api do
 				
 				expect(response     ).    to be_a Hash
 				expect(response.keys).not_to be_empty
-			end
-		end
-	end
-	
-	[
-		:capitalleagues,
-		:leagues,
-		:builderbaseleagues,
-		:warleagues,
-		:locations,
-		:goldpass_seasons_current,
-		:labels_players,
-		:labels_clans,
-	].each do |method_name|
-		describe "##{method_name}", vcr_cassette: method_name do
-			it 'returns a hash' do
-				response = api.send(method_name)
-				
-				expect(response).to be_a Hash
-			end
-			
-			it 'accepts query parameters' do
-				allow(api).to receive(:perform_request        ).once.with(any_args, query: {test: 5, asdf: 'i like trains'}, headers: nil, body: nil).and_return(success_response)
-				allow(api).to receive(:default_response_parser).and_return(donkey_transformer)
-				
-				expect(api.send(method_name, query: {test: 5, asdf: 'i like trains'})).to eq 'flying cows'
 			end
 		end
 	end
