246bef534062564fd5b2faf26ca0a9de0d7332d3
expeehaa <expeehaa@outlook.com>
2023-09-17 15:52:28 +0200

    Place endpoint method test arguments in an array to support variable lengths of arguments.

--- spec/clash_of_clans_api/api_spec.rb
+++ spec/clash_of_clans_api/api_spec.rb
@@ -26,54 +26,50 @@ RSpec.describe ClashOfClansApi::Api do
 	end
 	
 	[
-		[:clan_currentwar_leaguegroup,          nil,          'aninvalidvalue'],
-		[:clanwarleagues_war,                   nil,          'aninvalidvalue'],
-		[:clan_warlog,                          '#2YYQPVGQQ', 'aninvalidvalue'],
-		[:clan_currentwar,                      '#2YYQPVGQQ', 'aninvalidvalue'],
-		[:clan,                                 '#2YYQPVGQQ', 'aninvalidvalue'],
-		[:clan_members,                         '#2YYQPVGQQ', 'aninvalidvalue'],
-		[:clan_capitalraidseasons,              '#2YYQPVGQQ', 'aninvalidvalue'],
-		[:player,                               '#QG8VUCRUQ', 'aninvalidvalue'],
-		[:capitalleague,                        85000022,     'aninvalidvalue'],
-		[:league,                               29000000,     'aninvalidvalue'],
-		[:league_seasons,                       29000022,     'aninvalidvalue'], # League seasons are only available in the legendary league.
-		[:builderbaseleague,                    44000041,     'aninvalidvalue'],
-		[:warleague,                            48000000,     'aninvalidvalue'],
-		[:location_rankings_clans,              32000094,     'aninvalidvalue'],
-		[:location_rankings_players,            32000094,     'aninvalidvalue'],
-		[:location_rankings_clansversus,        32000094,     'aninvalidvalue'],
-		[:location_rankings_playersbuilderbase, 32000094,     'aninvalidvalue'],
-		[:location_rankings_clansbuilderbase,   32000094,     'aninvalidvalue'],
-		[:location_rankings_playersversus,      32000094,     'aninvalidvalue'],
-		[:location_rankings_capitals,           32000094,     'aninvalidvalue'],
-		[:location,                             32000000,     'aninvalidvalue'],
-	].each do |method_name, argument, invalid_argument|
+		[:clan_currentwar_leaguegroup,          nil,            ['aninvalidvalue']],
+		[:clanwarleagues_war,                   nil,            ['aninvalidvalue']],
+		[:clan_warlog,                          ['#2YYQPVGQQ'], ['aninvalidvalue']],
+		[:clan_currentwar,                      ['#2YYQPVGQQ'], ['aninvalidvalue']],
+		[:clan,                                 ['#2YYQPVGQQ'], ['aninvalidvalue']],
+		[:clan_members,                         ['#2YYQPVGQQ'], ['aninvalidvalue']],
+		[:clan_capitalraidseasons,              ['#2YYQPVGQQ'], ['aninvalidvalue']],
+		[:player,                               ['#QG8VUCRUQ'], ['aninvalidvalue']],
+		[:capitalleague,                        [85000022    ], ['aninvalidvalue']],
+		[:league,                               [29000000    ], ['aninvalidvalue']],
+		[:league_seasons,                       [29000022    ], ['aninvalidvalue']], # League seasons are only available in the legendary league.
+		[:builderbaseleague,                    [44000041    ], ['aninvalidvalue']],
+		[:warleague,                            [48000000    ], ['aninvalidvalue']],
+		[:location_rankings_clans,              [32000094    ], ['aninvalidvalue']],
+		[:location_rankings_players,            [32000094    ], ['aninvalidvalue']],
+		[:location_rankings_clansversus,        [32000094    ], ['aninvalidvalue']],
+		[:location_rankings_playersbuilderbase, [32000094    ], ['aninvalidvalue']],
+		[:location_rankings_clansbuilderbase,   [32000094    ], ['aninvalidvalue']],
+		[:location_rankings_playersversus,      [32000094    ], ['aninvalidvalue']],
+		[:location_rankings_capitals,           [32000094    ], ['aninvalidvalue']],
+		[:location,                             [32000000    ], ['aninvalidvalue']],
+	].each do |method_name, valid_arguments, invalid_arguments|
 		describe "##{method_name}", vcr_cassette: method_name do
-			it 'requires one argument' do
-				expect{api.send(method_name)}.to raise_error TypeError, 'cannot escape nil'
-			end
-			
 			it 'accepts query parameters' do
 				allow(api).to receive(:perform_request        ).once.with(any_args, query: {test: 5, asdf: 'i like trains'}, headers: nil, body: nil).and_return(success_response)
 				allow(api).to receive(:default_response_parser).and_return(donkey_transformer)
 				
-				expect(api.send(method_name, 'some_test_string', query: {test: 5, asdf: 'i like trains'})).to eq 'flying cows'
+				expect(api.send(method_name, ['some_test_string']*invalid_arguments.size, query: {test: 5, asdf: 'i like trains'})).to eq 'flying cows'
 			end
 			
 			it 'raises an error with an invalid argument' do
-				expect{api.send(method_name, invalid_argument)}.to raise_error do |error|
+				expect{api.send(method_name, *invalid_arguments)}.to raise_error do |error|
 					expect(error         ).to be_a ApiFrame::NoSuccessError
 					expect(error.response).to be_a(Net::HTTPNotFound).or be_a(Net::HTTPBadRequest)
 				end
 			end
 			
-			if !argument.nil?
-				it 'returns a Hash with a valid argument' do
-					response = api.send(method_name, argument)
-					
-					expect(response     ).    to be_a Hash
-					expect(response.keys).not_to be_empty
-				end
+			it 'returns a Hash with a valid argument' do
+				pending "Replace nil with an array of valid arguments for method #{method_name}!" if valid_arguments.nil?
+				
+				response = api.send(method_name, *valid_arguments)
+				
+				expect(response     ).    to be_a Hash
+				expect(response.keys).not_to be_empty
 			end
 		end
 	end
