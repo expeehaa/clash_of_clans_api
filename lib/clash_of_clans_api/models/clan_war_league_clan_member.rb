c7307b7284ebeb885eeb035edc92cf9daa9adbe4
expeehaa <expeehaa@outlook.com>
2025-04-06 03:33:39 +0200

    Fix alignment.

--- lib/clash_of_clans_api/models/clan_war_league_clan_member.rb
+++ lib/clash_of_clans_api/models/clan_war_league_clan_member.rb
@@ -3,8 +3,8 @@ require_relative 'base'
 module ClashOfClansApi
 	module Models
 		class ClanWarLeagueClanMember < Base
-			property :tag,            'tag',           required: true
-			property :name,           'name',          required: true
+			property :tag,             'tag',           required: true
+			property :name,            'name',          required: true
 			property :town_hall_level, 'townHallLevel', required: true
 		end
 	end
