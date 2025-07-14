f38baaa756f0d48e63092bb0b31fbe9176d8f753
expeehaa <expeehaa@outlook.com>
2025-07-14 16:40:47 +0200

    Make test work on Ruby 3.0.

--- spec/clash_of_clans_api/utils_spec.rb
+++ spec/clash_of_clans_api/utils_spec.rb
@@ -17,7 +17,7 @@ RSpec.describe ClashOfClansApi::Utils do
 		it 'ensures that the answer from ipv4.icanhazip.com is a valid IPv4 address' do
 			stub_request(:get, 'https://ipv4.icanhazip.com').to_return(body: 'not an IP address')
 			
-			expect{ClashOfClansApi::Utils.current_ipv4_address}.to raise_error IPAddr::InvalidAddressError, 'invalid address: '
+			expect{ClashOfClansApi::Utils.current_ipv4_address}.to raise_error IPAddr::InvalidAddressError, /invalid address: /
 		end
 	end
 end
